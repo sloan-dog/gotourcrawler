bd28717b2c25f4d7d4719106ea21c0365c61ae87
prov-sloan <sloan@WALT953442.local>
2018-03-12 16:06:48 -0700

    cleaned up previous attempt

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 )
 
 type Fetcher interface {
@@ -10,29 +12,63 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+type UrlCache struct {
+	sync.Mutex
+	urls map[string]bool
+}
+
+func (urlc *UrlCache) IsVisited(url string) bool {
+	urlc.Lock()
+	defer urlc.Unlock()
+	_, exists := urlc.urls[url]
+	return exists == true
+}
+
+func (urlc *UrlCache) VisitUrl(url string) {
+	urlc.Lock()
+	defer urlc.Unlock()
+	urlc.urls[url] = true
+}
+
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
-	// This implementation doesn't do either:
+func Crawl(url string, depth int, fetcher Fetcher, urlc *UrlCache, wg *sync.WaitGroup, name int) {
+	defer wg.Done()
 	if depth <= 0 {
 		return
 	}
-	body, urls, err := fetcher.Fetch(url)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if !urlc.IsVisited(url) {
+		body, urls, err := fetcher.Fetch(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		urlc.VisitUrl(url)
+		fmt.Printf("name: %v, urls: %v\n", name, urls)
+		fmt.Printf("name: %v, found: %s %q\n", name, url, body)
+		fmt.Printf("num routines: %v\n", runtime.NumGoroutine())
+		for i, u := range urls {
+			wg.Add(1)
+			go func(_u string, _i int) {
+				Crawl(_u, depth-1, fetcher, urlc, wg, name+i+20)
+				// bind parameters to this closure
+				// otherwise, Crawl call is bound to the u reference, and will likely be end of loop value
+			}(u, i)
+		}
+	} else {
+		fmt.Printf("name: %v ignoring existing url: %v\n", name, url)
 	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
-	}
-	return
 }
 
 func main() {
-	Crawl("https://golang.org/", 4, fetcher)
+	var wg sync.WaitGroup
+	urlc := &UrlCache{
+		urls: map[string]bool{},
+	}
+	wg.Add(1)
+	name := 0
+	go Crawl("https://golang.org/", 4, fetcher, urlc, &wg, name)
+	wg.Wait()
 }
 
 // fakeFetcher is Fetcher that returns canned results.
